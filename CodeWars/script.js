ddeb20ae58ddbfe335234662102021ad511c04e5
xOAviOx <avishuklacode@gmail.com>
2024-08-10 23:59:43 +0530

    Update script.js

--- CodeWars/script.js
+++ CodeWars/script.js
@@ -8,7 +8,7 @@
 // makeNegative(0.12); // return -0.12
 // Notes
 // The number can be negative already, in which case no change is required.
-// Zero (0) is not checked for any specific sign. Negative zeros make no mathematical sense.
+// Zero (0) is nod t checked for any specific sign. Negative zeros make no mathematical sense.
 
 Answer 1.function makeNegative(num) {
   // Code?
