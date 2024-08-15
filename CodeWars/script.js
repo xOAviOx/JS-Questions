688b8a299978effd6f0a6375a62659ed903353c4
xOAviOx <avishuklacode@gmail.com>
2024-08-15 23:39:36 +0530

    Update script.js

--- CodeWars/script.js
+++ CodeWars/script.js
@@ -141,5 +141,20 @@ Answer.const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
 
 console.log(zeroFuel(50,25,2));
   
+//9. We need a function that can transform a number (integer) into a string.
+
+// What ways of achieving this do you know?
+
+// Examples (input --> output):
+// 123  --> "123"
+// 999  --> "999"
+// -100 --> "-100"
+Answers. 
+function numberToString(num) {
+  // Return a string of the number here!
+  const stringNum = String(num)
+  return stringNum
+}
+console.log(numberToString(54));
 
 
